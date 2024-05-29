63959f1d3d68510f4941e9cc0d4e8b8335134b05
Ben White <ben@posthog.com>
2024-05-29 12:18:57 +0100

    Fixes

--- index.test.ts
+++ index.test.ts
@@ -2,24 +2,53 @@ import { PostHogEvent, Webhook } from '@posthog/plugin-scaffold'
 
 const { composeWebhook } = require('./index')
 
-test('creates the correct webhook body', async () => {
-    const event: PostHogEvent = {
-        uuid: '123',
-        team_id: 1,
-        distinct_id: 'alice',
-        event: 'pageview',
-        timestamp: new Date('2023-01-01T00:00:00.000Z'),
-        properties: {
-            prop: 'value',
-        },
-    }
-    const webhook: Webhook = composeWebhook(event, { config: { url: 'https://example.com' } })
-    expect(webhook).toEqual({
-        url: 'https://example.com',
-        body: '{"uuid":"123","team_id":1,"distinct_id":"alice","event":"pageview","timestamp":"2023-01-01T00:00:00.000Z","properties":{"prop":"value"}}',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        method: 'POST',
+const event: PostHogEvent = {
+    uuid: '123',
+    team_id: 1,
+    distinct_id: 'alice',
+    event: 'pageview',
+    timestamp: new Date('2023-01-01T00:00:00.000Z'),
+    properties: {
+        prop: 'value',
+    },
+}
+
+describe('webhook plugin', () => {
+    it('should have sensible defaults', () => {
+        const webhook: Webhook = composeWebhook(event, { config: { url: 'https://example.com' } })
+        expect(webhook).toMatchInlineSnapshot(`
+            Object {
+              "body": "{\\"uuid\\":\\"123\\",\\"team_id\\":1,\\"distinct_id\\":\\"alice\\",\\"event\\":\\"pageview\\",\\"timestamp\\":\\"2023-01-01T00:00:00.000Z\\",\\"properties\\":{\\"prop\\":\\"value\\"}}",
+              "headers": Object {
+                "Content-Type": "application/json",
+              },
+              "method": "POST",
+              "url": "https://example.com",
+            }
+        `)
+    })
+
+    it('should allow overriding config options', () => {
+        const webhook: Webhook = composeWebhook(event, {
+            config: {
+                url: 'https://example.com',
+                payload: { my: 'payload' },
+                headers: {
+                    'X-My-Header': 'my-value',
+                },
+                method: 'PUT',
+            },
+        })
+        expect(webhook).toMatchInlineSnapshot(`
+            Object {
+              "body": "{\\"my\\":\\"payload\\"}",
+              "headers": Object {
+                "Content-Type": "application/json",
+                "X-My-Header": "my-value",
+              },
+              "method": "PUT",
+              "url": "https://example.com",
+            }
+        `)
     })
 })
